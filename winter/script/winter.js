f25a097f562aac953d99e3a52c16a62395f76879
csanders891 <65356624+csanders891@users.noreply.github.com>
2025-02-01 19:10:47 -0600

    Add files via upload

---
+++ winter/script/winter.js
@@ -0,0 +1,10 @@
+var years = [1924, 1928, 1932, 1936, 1940, 1944, 1948, 1952, 1956, 1960, 1964, 1968, 1972, 1976, 1980, 1984, 1988, 1992, 1994, 1998, 2002, 2006, 2010, 2014, 2018, 2022, 2026, 2030, 2034];
+
+var hostCities = ["Chamonix", "St. Mortiz", "Lake Placid", "Garmisch-Partenkirchen", "cancelled due to World War 2", "cancelled due to World War 2", "St. Mortiz", "Oslo", "Cortina d'Ampezzo", "Squaw Valley", "Innsbruck", "Grenoble", "Sapporo", "Innsbruck", "Lake Placid", "Sarajevo", "Calgary", "Albertville", "Lillehammer", "Nagano", "Salt Lake City", "Turin", "Vancouver", "Sochi", "Pyeonchang", "Beijing", "Milan and Cortina d'Ampezzo", "Nice and French Alps", "Salt Lake City"];
+
+for(var i = 0; i < years.length; i++)
+    alert(years[i] + " \n" + hostCities[i]);		
+
+alert("That is all.");
+
+
